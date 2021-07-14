59582d7ce302b13f68ee601f7c18f70cd6cbfb6d
Tiago Moura <tiagoms.dev@gmail.com>
2021-07-14 12:23:46 -0400

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
 import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+function ProfileSideBar(prop){
+  return (
+    <Box>
+          <img src={`https://github.com/${prop.githubUser}.png`} style={{borderRadius: '8px'}}></img>
+    </Box>
+  )
+}
+
 export default function Home() {
-  return <Title>My page</Title>
+  const user = 'peas';
+  const pessoasFavoritas = ['juunegreiros','omariosouto','peas','rafaballerini','marcobrunodev','felipefialho']
+
+  return (
+    <>
+    <AlurakutMenu/>
+    <MainGrid>
+      <div className="profileArea" style={{gridArea: 'profileArea'}}>
+        <ProfileSideBar githubUser={user}/>
+      </div>
+      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+        <Box>
+          <h1 className="title">
+            Bem vindo(a)
+          </h1>
+          <OrkutNostalgicIconSet/>
+        </Box>
+      </div>  
+      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">
+            Pessoas da Comunidade ({pessoasFavoritas.length})
+          </h2>
+          <ul>
+            {pessoasFavoritas.map((item) => {
+              return (
+                <li>
+                  <a href={`/users/${item}`} key={item}>
+                    <img src={`https://github.com/${item}.png`}/>
+                    <span>{item}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>
+    </MainGrid>
+    </>
+  )
 }
